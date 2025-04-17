65060be09a751515f275300e9dfd181b64350f40
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-18 00:27:29 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Consisco - AI-powered PDF Summarization",
+  description: "Consisco is a platform for summarizing pdf documents.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
