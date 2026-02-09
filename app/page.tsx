ee84ab6579d21821bb500ad4bc4ca381c52e0dc2
siddhant1232 <siddhantgupta797400@gmail.com>
2026-02-09 21:54:45 +0530

    instructions added

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <div className="relative w-full">
       <BgGradient />
+       {/* // background gradient */}
       <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
